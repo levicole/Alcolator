8d149228ff1643329633e7b950f784acd4ae5371
Levi Kennedy <lckennedy@gmail.com>
2015-06-02 18:17:39 -0500

    Checkpoint 20 completed

---
+++ Alcolator/MainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuViewController.h
+//  Alcolator
+//
+//  Created by Levi Kennedy on 6/2/15.
+//  Copyright (c) 2015 Levi Kennedy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainMenuViewController : UIViewController
+
+@end
